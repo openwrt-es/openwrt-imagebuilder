8f776e70fd83143d8467084b5e76767d61305947
Álvaro Fernández Rojas <noltari@gmail.com>
2021-04-21 14:05:40 +0200

    Add support for generating RPi 4 images
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ raspberrypi-router.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. lib/ib.sh
+
+function main() {
+	release_version "21.02-SNAPSHOT"
+	firmware_packages "bird2 qrencode \
+		kmod-hwmon-gpiofan kmod-hwmon-pwmfan kmod-usb-net-rtl8152 \
+		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard"
+	firmware_files "files/rpi-router"
+
+	prepare_imagebuilder "bcm27xx" "bcm2711"
+	build_firmware "bcm27xx/bcm2711" "rpi-4" "openwrt-*-rpi-4*.*"
+}
+
+main
+
+exit 0
