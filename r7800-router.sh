0d76a38006dea58b566cf7db4488f0df6d789948
Álvaro Fernández Rojas <noltari@gmail.com>
2021-05-12 09:56:49 +0200

    r7800: add 21.02 snapshots with router packages
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ r7800-router.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. lib/ib.sh
+
+function main() {
+	release_version "21.02-SNAPSHOT"
+	firmware_packages "ath10k-firmware-qca9984-ct-full-htt bird2 qrencode \
+		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard"
+	firmware_files "files/ipq806x"
+
+	prepare_imagebuilder "ipq806x" "generic"
+	build_firmware "ipq806x/generic" "netgear_r7800" "openwrt-*-netgear_r7800*.*"
+}
+
+main
+
+exit 0
