ad9114575ab22246ef02c31469ebe7d4ef3cbb6d
Álvaro Fernández Rojas <noltari@gmail.com>
2021-06-08 13:28:17 +0200

    wdr4300: update to OpenWrt 21.02 snapshots
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- wdr4300.sh
+++ wdr4300.sh
@@ -3,8 +3,8 @@
 . lib/ib.sh
 
 function main() {
-	release_version "snapshots"
-	firmware_packages "luci luci-proto-ipv6"
+	release_version "21.02-SNAPSHOT"
+	firmware_packages "luci luci-app-ddns"
 
 	prepare_imagebuilder "ar71xx" "generic"
 	build_firmware "ar71xx/generic" "tl-wdr4300-v1" "openwrt-*-tl-wdr4300*.*"
