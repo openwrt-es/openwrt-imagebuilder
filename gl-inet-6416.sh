e636c06791af946e28f3965c2e098b9638b025c6
Álvaro Fernández Rojas <noltari@gmail.com>
2021-12-19 22:47:46 +0100

    gl-inet-6416: add new script
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ gl-inet-6416.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. lib/ib.sh
+
+function main() {
+	release_version "snapshots"
+	firmware_packages "luci"
+
+	prepare_imagebuilder "ath79" "generic"
+	build_firmware "ath79/generic" "glinet_6416" "openwrt-*-glinet_6416-*.*"
+}
+
+main
+
+exit 0
