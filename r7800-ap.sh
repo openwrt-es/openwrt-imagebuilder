aa09d767841365632b5439202e7daca5a71fe9a5
Álvaro Fernández Rojas <noltari@gmail.com>
2021-12-19 23:06:51 +0100

    r7800-ap: add script for R7800 as Dumb AP
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ r7800-ap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. lib/ib.sh
+
+function main() {
+	release_version "21.02-SNAPSHOT"
+	firmware_packages "ath10k-firmware-qca9984-ct-full-htt luci"
+
+	prepare_imagebuilder "ipq806x" "generic"
+	build_firmware "ipq806x/generic" "netgear_r7800" "openwrt-*-netgear_r7800*.*"
+}
+
+main
+
+exit 0
