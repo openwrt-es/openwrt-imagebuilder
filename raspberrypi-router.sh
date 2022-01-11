0d4edcbfae8d0c75ea73390724c0fd5b54a0da63
Álvaro Fernández Rojas <noltari@gmail.com>
2022-01-11 08:59:53 +0100

    raspberrypi-router: add irqbalance and htop
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- raspberrypi-router.sh
+++ raspberrypi-router.sh
@@ -5,6 +5,7 @@
 function main() {
 	release_version "snapshots"
 	firmware_packages "bcm27xx-userland bird2 qrencode \
+		irqbalance htop \
 		kmod-hwmon-gpiofan kmod-hwmon-pwmfan kmod-usb-net-rtl8152 \
 		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard"
 	firmware_files "files/rpi-router"
