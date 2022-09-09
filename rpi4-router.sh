ebb0ab26719348ef1960fce5113eede3405d2d1d
Álvaro Fernández Rojas <noltari@gmail.com>
2022-09-09 12:30:30 +0200

    rpi4: v22.03.0 + rtl8152 v2.16.3
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi4-router.sh
+++ rpi4-router.sh
@@ -3,11 +3,12 @@
 . lib/ib.sh
 
 function main() {
-	release_version "snapshots"
+	release_version "22.03.0"
+	custom_packages "packages/rpi4/22.03.0/kmod-usb-net-rtl8152-vendor_5.10.138+2.16.3-bcm27xx-1_aarch64_cortex-a72.ipk"
 	firmware_packages "bcm27xx-userland bird2 qrencode \
 		irqbalance htop \
 		kmod-hwmon-gpiofan kmod-hwmon-pwmfan \
-		ethtool kmod-usb-net-rtl8152 usbutils \
+		ethtool kmod-usb-net-rtl8152-vendor usbutils \
 		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard"
 	firmware_files "files/rpi-router"
 
