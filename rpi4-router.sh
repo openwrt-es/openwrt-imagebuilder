521f02ec4825d1814b18f237cf439eeed9db538c
Álvaro Fernández Rojas <noltari@gmail.com>
2022-09-13 11:03:04 +0200

    rpi4-router: add docker packages
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi4-router.sh
+++ rpi4-router.sh
@@ -8,8 +8,13 @@ function main() {
 		irqbalance htop \
 		kmod-hwmon-gpiofan kmod-hwmon-pwmfan \
 		ethtool kmod-usb-net-rtl8152 usbutils \
-		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard"
+		luci luci-app-ddns luci-app-sqm luci-app-upnp luci-app-wireguard \
+		docker dockerd docker-compose luci-app-dockerman \
+		kmod-usb-storage kmod-usb-storage-uas usbutils \
+		block-mount libblkid \
+		gdisk e2fsprogs kmod-fs-ext4 kmod-fs-f2fs f2fs-tools"
 	firmware_files "files/rpi-router"
+	firmware_rootfs_partsize "1024"
 
 	prepare_imagebuilder "bcm27xx" "bcm2711"
 	build_firmware "bcm27xx/bcm2711" "rpi-4" "openwrt-*-rpi-4*.*"
