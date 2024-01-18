bd5e7b7c1c2770b683bd5b8b979eafae02f513a8
Álvaro Fernández Rojas <noltari@gmail.com>
2024-01-18 13:15:32 +0100

    rpi4-router: switch to OpenWrt 23.05.2
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi4-router.sh
+++ rpi4-router.sh
@@ -3,7 +3,7 @@
 . lib/ib.sh
 
 function main() {
-	release_version "snapshots"
+	release_version "23.05.2"
 	firmware_packages "bcm27xx-userland bird2 qrencode \
 		irqbalance htop mosquitto-client-nossl \
 		kmod-hwmon-gpiofan kmod-hwmon-pwmfan \
