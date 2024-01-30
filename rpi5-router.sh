07d399888fd1249e1532b6667827e4c73725021d
Álvaro Fernández Rojas <noltari@gmail.com>
2024-01-30 08:24:02 +0100

    Add script for Raspberry Pi 5 router
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

---
+++ rpi5-router.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. lib/ib.sh
+
+function main() {
+	release_version "snapshots"
+	firmware_packages "bird2 qrencode \
+		irqbalance htop mosquitto-client-nossl \
+		ethtool kmod-usb-net-rtl8152 usbutils \
+		luci luci-app-sqm luci-app-upnp luci-app-wireguard \
+		luci-app-ddns wget-ssl drill"
+	firmware_files "files/rpi5-router"
+
+	prepare_imagebuilder "bcm27xx" "bcm2712"
+	build_firmware "bcm27xx/bcm2712" "rpi-5" "openwrt-*-rpi-5*.*"
+}
+
+main
+
+exit 0
