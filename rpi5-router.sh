73786929c82da3912d56b06420319cb72addfe3b
Álvaro Fernández Rojas <noltari@gmail.com>
2024-05-09 11:18:39 +0200

    rpi5-router: add kmod-usb-net-asix-ax88179
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi5-router.sh
+++ rpi5-router.sh
@@ -6,7 +6,8 @@ function main() {
 	release_version "snapshots"
 	firmware_packages "bird2 qrencode \
 		irqbalance htop mosquitto-client-nossl \
-		ethtool kmod-r8169 kmod-usb-net-rtl8152 usbutils \
+		ethtool kmod-r8169 \
+		kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 usbutils \
 		luci luci-app-sqm luci-app-upnp luci-app-wireguard \
 		luci-app-ddns wget-ssl drill"
 	firmware_files "files/rpi5-router"
