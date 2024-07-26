6e94a63ca0a42f811f9cdfebc18e72018a52e254
Álvaro Fernández Rojas <noltari@gmail.com>
2024-07-26 14:14:42 +0200

    Update to OpenWrt v23.05.4
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- r7800-router.sh
+++ r7800-router.sh
@@ -3,7 +3,7 @@
 . lib/ib.sh
 
 function main() {
-	release_version "23.05.2"
+	release_version "23.05.4"
 	firmware_packages "ath10k-firmware-qca9984-ct-full-htt bird2 qrencode \
 		luci luci-app-sqm luci-app-upnp luci-app-wireguard \
 		luci-app-ddns wget-ssl drill"
--- rpi4-router.sh
+++ rpi4-router.sh
@@ -3,7 +3,7 @@
 . lib/ib.sh
 
 function main() {
-	release_version "23.05.2"
+	release_version "23.05.4"
 	firmware_packages "bcm27xx-userland bird2 qrencode \
 		irqbalance htop mosquitto-client-nossl \
 		kmod-hwmon-gpiofan kmod-hwmon-pwmfan \
