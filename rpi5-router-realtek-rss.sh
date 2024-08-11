85b7c911a253520abb2adc7e3f39f2da6abdbfb1
Álvaro Fernández Rojas <noltari@gmail.com>
2024-08-11 17:48:47 +0200

    package: remove upstreamed realtek packages
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi5-router-realtek-rss.sh
+++ rpi5-router-realtek-rss.sh
@@ -7,16 +7,13 @@ function main() {
 	release_version "snapshots"
 	firmware_packages "bird2 qrencode \
 		irqbalance htop mosquitto-client-nossl \
-		ethtool kmod-r8126 \
+		ethtool kmod-r8168 kmod-r8125-rss kmod-r8126-rss \
 		kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 usbutils \
 		luci luci-app-sqm luci-app-upnp luci-proto-wireguard \
 		luci-app-ddns wget-ssl drill"
 	firmware_files "files/rpi5-router"
 
 	prepare_imagebuilder "bcm27xx" "bcm2712"
-	prepare_sdk "bcm27xx" "bcm2712" "gcc-13.3.0_musl"
-
-	build_sdk_packages "bcm27xx/bcm2712" "rpi-5" "r8126"
 	build_firmware "bcm27xx/bcm2712" "rpi-5" "openwrt-*-rpi-5*.*"
 }
 
