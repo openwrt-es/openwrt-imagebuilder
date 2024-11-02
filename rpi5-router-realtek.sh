2e7c966cb608bb9b9431e9090e5d4019612c118c
Álvaro Fernández Rojas <noltari@gmail.com>
2024-11-02 23:50:20 +0100

    rpi5-router: increase rootfs size to 256M
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi5-router-realtek.sh
+++ rpi5-router-realtek.sh
@@ -15,6 +15,7 @@ function main() {
 		luci luci-app-sqm luci-app-upnp luci-proto-wireguard \
 		luci-app-ddns wget-ssl drill"
 	firmware_files "files/rpi5-router"
+	firmware_rootfs_partsize 256
 
 	prepare_imagebuilder "bcm27xx" "bcm2712"
 	build_firmware "bcm27xx/bcm2712" "rpi-5" "openwrt-*-rpi-5*.*"
