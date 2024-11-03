52d097bbb4b9a98834e0e36b1a018b84c43affd7
Álvaro Fernández Rojas <noltari@gmail.com>
2024-11-03 21:44:27 +0100

    archer-c7: add luci-app-ddns
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- archer-c7.sh
+++ archer-c7.sh
@@ -6,7 +6,7 @@ function main() {
 	release_tar_zst
 	release_version "snapshots"
 
-	firmware_packages "luci"
+	firmware_packages "luci luci-app-ddns"
 
 	prepare_imagebuilder "ath79" "generic"
 	build_firmware "ath79/generic" "tplink_archer-c7-v2" "openwrt-*-tplink_archer-c7-v2*.*"
