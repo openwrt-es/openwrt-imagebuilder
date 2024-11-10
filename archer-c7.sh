9170b6c9fc312178c50e1e25a927709b7d26b6fc
Álvaro Fernández Rojas <noltari@gmail.com>
2024-11-10 17:50:57 +0100

    archer-c7: add wget-ssl and drill packages
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- archer-c7.sh
+++ archer-c7.sh
@@ -8,7 +8,7 @@ function main() {
 	release_tar_zst
 	release_version "24.10-SNAPSHOT"
 
-	firmware_packages "luci luci-app-ddns"
+	firmware_packages "luci luci-app-ddns wget-ssl drill"
 
 	prepare_imagebuilder "ath79" "generic"
 	build_firmware "ath79/generic" "tplink_archer-c7-v2" "openwrt-*-tplink_archer-c7-v2*.*"
