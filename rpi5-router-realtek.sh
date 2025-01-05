8e9db1c6bb1a0b1ff08de495948cb479fd8bb9c1
Álvaro Fernández Rojas <noltari@gmail.com>
2025-01-05 21:05:56 +0100

    rpi5-router: add luci-app-banip kmod-rp1-adc
    Also switch to 24.10 snapshots for kmod-rp1-adc.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- rpi5-router-realtek.sh
+++ rpi5-router-realtek.sh
@@ -4,14 +4,15 @@
 
 function main() {
 	release_tar_zst
-	release_version "24.10.0-rc4"
+	release_version "24.10-SNAPSHOT"
 
 	firmware_packages "luci-ssl luci-app-uhttpd \
 		luci-app-ddns wget-ssl drill \
 		luci-proto-wireguard qrencode \
-		luci-app-sqm luci-app-upnp \
+		luci-app-banip luci-app-sqm luci-app-upnp \
 		luci-app-omcproxy luci-app-udpxy \
 		bird2 ethtool htop mosquitto-client-nossl \
+		kmod-rp1-adc \
 		kmod-r8168 kmod-r8125 kmod-r8126 \
 		kmod-ipt-nathelper-rtsp \
 		kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 usbutils"
