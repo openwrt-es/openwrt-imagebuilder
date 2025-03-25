6733bc6607ea4ab711c9f53febe7ddcd3b9227e5
Álvaro Fernández Rojas <noltari@gmail.com>
2025-03-25 08:09:33 +0100

    r7800-router: add IPv6 and utils
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- r7800-router.sh
+++ r7800-router.sh
@@ -9,8 +9,9 @@ function main() {
 	firmware_packages "luci-ssl luci-app-uhttpd \
 		luci-app-ddns wget-ssl drill \
 		luci-proto-wireguard qrencode \
+		luci-proto-ipv6 6in4 kmod-ipt-nat6 \
 		luci-app-banip luci-app-sqm luci-app-upnp \
-		bird2 ethtool htop mosquitto-client-nossl \
+		bird2 ethtool htop mosquitto-client-nossl mtr \
 		-ath10k-firmware-qca9984-ct ath10k-firmware-qca9984 \
 		-kmod-ath10k-ct kmod-ath10k"
 	firmware_files "files/r7800-router"
