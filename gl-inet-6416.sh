bb55f329169bd1be9a94d6cc942b950a1b9a5c80
Álvaro Fernández Rojas <noltari@gmail.com>
2025-05-24 18:16:07 +0200

    gl-inet-6416: switch to OpenWrt 24.10-SNAPSHOT
    Add more packages.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- gl-inet-6416.sh
+++ gl-inet-6416.sh
@@ -4,9 +4,12 @@
 
 function main() {
 	release_tar_zst
-	release_version "snapshots"
+	release_version "24.10-SNAPSHOT"
 
-	firmware_packages "luci"
+	firmware_packages "luci-ssl luci-app-uhttpd \
+		luci-proto-ipv6 \
+		htop \
+		kmod-mt7921u"
 
 	prepare_imagebuilder "ath79" "generic"
 	build_firmware "ath79/generic" "glinet_6416" "openwrt-*-glinet_6416-*.*"
