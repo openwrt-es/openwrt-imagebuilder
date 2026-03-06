b1a9e3d647b023a2628181fdd6fe159bfc729b76
Álvaro Fernández Rojas <noltari@gmail.com>
2026-03-06 12:29:18 +0100

    r7800: several improvements
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- r7800-router.sh
+++ r7800-router.sh
@@ -9,9 +9,10 @@ function main() {
 	firmware_packages "luci-ssl luci-app-uhttpd \
 		luci-app-ddns wget-ssl drill \
 		luci-proto-wireguard qrencode \
-		luci-proto-ipv6 6in4 kmod-ipt-nat6 \
+		luci-proto-ipv6 kmod-ipt-nat6 \
 		luci-app-banip luci-app-sqm luci-app-upnp \
-		bird3 ethtool htop mosquitto-client-nossl mtr \
+		bird3 btop ethtool-full htop mosquitto-client-nossl \
+		mtr rpcapd tcpdump \
 		-ath10k-firmware-qca9984-ct ath10k-firmware-qca9984 \
 		-kmod-ath10k-ct kmod-ath10k \
 		block-mount kmod-usb-storage usbutils \
